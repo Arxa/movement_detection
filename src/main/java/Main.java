5bfda5e9794fc0a851b26c5862610f0a6915a1f8
Arxa <nikiarch@teiser.gr>
2018-06-25 19:18:52 +0300

    added linux OpenCV support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,10 @@ import spark.Spark;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         Utils.loadNatives();
         Camera.openCamera();
+        //Spark.port(8080);
         Spark.staticFiles.location("/web");
         Spark.staticFiles.expireTime(600);
         // websocket path should be the same as the one defined in webSocket.js
--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,11 +1,38 @@
+import org.apache.commons.lang3.SystemUtils;
+
+import java.io.File;
+import java.nio.file.Paths;
 
 public class Utils {
 
-    public static void loadNatives(){
-        try {
-            System.load(Utils.class.getResource("natives/opencv_java320_64.dll").toURI().getPath());
-        } catch (Exception e) {
-            System.out.println("Failed to load native file: " + e.getMessage());
+    public static void loadNatives() throws Exception
+    {
+        String RESOURCES_NATIVES = Paths.get(new File(".").getCanonicalPath(),
+                "src","main","resources","natives").toFile().getPath();
+
+        if(SystemUtils.IS_OS_WINDOWS)
+        {
+            int bit = Integer.parseInt(System.getProperty("sun.arch.data.model"));
+            if(bit == 32){
+                System.load(Paths.get(RESOURCES_NATIVES,"opencv_320_32.dll").toString());
+                System.out.println("Loaded OpenCV for Windows 32 bit");
+            }
+            else if (bit == 64){
+                System.load(Paths.get(RESOURCES_NATIVES,"opencv_java320_64.dll").toString());
+                System.out.println("Loaded OpenCV for Windows 64 bit");
+            }
+            else{
+                System.out.println("Unknown Windows bit - trying with 32");
+                System.load(Paths.get(RESOURCES_NATIVES,"opencv_java320_32.dll").toString());
+                System.out.println("Loaded OpenCV for Windows 32 bit");
+            }
+        }
+        else if(SystemUtils.IS_OS_MAC){
+            System.out.println("MAC OS IS NOT SUPPORTED");
+        }
+        else if(SystemUtils.IS_OS_LINUX){
+            System.load(Paths.get(RESOURCES_NATIVES,"libopencv_320_64.so").toString());
+            System.out.println("Loaded OpenCV for Linux 64 bit\n");
         }
     }
 }
